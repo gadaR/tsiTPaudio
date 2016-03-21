c466e8618e8ec6e4ea9e5fb3931edd48df634745
AL3XEY <allemagne7@hotmail.fr>
2016-03-21 22:58:20 +0100

    Script pour affichage modules

---
+++ module.m
@@ -0,0 +1,21 @@
+Fe = 16000;
+Te = 1/Fe;
+f = [1:Fe];
+D=1;
+%D=2;
+mod = 2+2*cos(2*pi*f*Te*D); %this is |H(f)|²
+
+figure(1);
+plot(f, mod, 'b');
+xlabel('f');
+ylabel('|H(f)|²');
+axis([1 Fe 0 5]);
+
+f = [0 Fe/2 Fe];
+mod = 2+2*cos(2*pi*f*Te*D);
+
+figure(2);
+stem(f, mod, '--b');
+xlabel('f');
+ylabel('|H(f)|²');
+axis([1 Fe 0 5])
