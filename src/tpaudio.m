288ce7a2bd8d4398176fecd3985993f961aea558
AL3XEY <allemagne7@hotmail.fr>
2016-03-25 00:40:20 +0100

    Correction de code + nouveaux exemples

--- src/tpaudio.m
+++ src/tpaudio.m
@@ -2,7 +2,7 @@ clc;	% Clear command window.
 clear;	% Delete all variables.
 close all;
 
-[Signal,Fe]= audioread('coucou.wav') ;
+[Signal,Fe]= audioread('../results/coucou/coucou.wav') ;
 % soundsc(Signal/max(Signal),Fe) ;
 
 N=length(Signal);
@@ -31,16 +31,24 @@ imp(1)=1;
 
 D=32;
 passBas = passeBas(D,Signal);
+audiowrite('../results/coucou/coucouPasseBasD32.wav', passBas/max(passBas), Fe);
 % pause;
 % 
-% soundsc(res/max(res),Fe) ;
+% soundsc(passBas/max(passBas),Fe) ;
 
 %2. Filtre en peigne
 
 %y(k)=x(k)+x(k?D)
-D=5000
 
-peign = peigne (D,Signal);
+D=20;
+peign = peigne(D,Signal);
+audiowrite('../results/coucou/coucouPeigneD20.wav', peign, Fe);
+D=3000;
+peign = peigne(D,Signal);
+audiowrite('../results/coucou/coucouPeigneD3000.wav', peign, Fe);
+D=5000;
+peign = peigne(D,Signal);
+audiowrite('../results/coucou/coucouPeigneD5000.wav', peign, Fe);
 % pause;
 % soundsc(peign/max(peign),Fe) ;
 
@@ -49,13 +57,21 @@ peign = peigne (D,Signal);
 
 D=20;
 ptout = passeTout(D,Signal);
+audiowrite('../results/coucou/coucouPasseToutD20.wav', ptout, Fe);
+D=3000;
+ptout = passeTout(D,Signal);
+audiowrite('../results/coucou/coucouPasseToutD3000.wav', ptout, Fe);
+D=5000;
+ptout = passeTout(D,Signal);
+audiowrite('../results/coucou/coucouPasseToutD5000.wav', ptout, Fe);
 % pause;
 % soundsc(res/max(res),Fe) ;
 
 %Reverberation 
 mat= zeros (6,N);
 for i=1:6
-    mat(i,:)= peigne(i*500,passBas);
+    sig = peigne(i*500,passBas);
+    mat(i,1:N)= sig(1:N);
 end
 
 res = sum(mat)/6;
@@ -65,6 +81,7 @@ res = passeTout(1000,res);
 
 % pause;
 soundsc(res/max(res),Fe) ;
+audiowrite('../results/coucou/coucouReverb.wav', res/max(res), Fe);
 
 %Affichage des fonctions de transfert
 
@@ -79,7 +96,7 @@ title('transformee')
 figure
 module = abs(fourrierTransform) ;
 plot(f,module)
-title('Module du passe tou D=20')
+title('Module du passe tout D=20')
 xlabel('f');
 ylabel('|H(f)|');
 
